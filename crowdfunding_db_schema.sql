9c1a6cdf6fb16af2f6c9f71623c84c7270b34bbc
ricsalazar810 <ricsalazar810@gmail.com>
2024-09-23 21:28:48 -0500

    Update crowdfunding_db_schema.sql

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -1,7 +1,3 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
-
-
 CREATE TABLE "Category" (
     "category_id" VARCHAR   NOT NULL,
     "category" VARCHAR   NOT NULL,
